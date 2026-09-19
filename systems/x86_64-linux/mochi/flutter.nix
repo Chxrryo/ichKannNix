ca0e7b47ebb8ef2cf621b1eb27f734061bf71a45
Chxrryo <alina.pollich4@gmail.com>
2026-09-19 19:44:02 +0100

    make libx11 available

--- systems/x86_64-linux/mochi/flutter.nix
+++ systems/x86_64-linux/mochi/flutter.nix
@@ -1,7 +1,12 @@
 { pkgs, ...}:
 
 {
-  programs.nix-ld.enable = true;
+  programs.nix-ld = {
+    enable = true;
+    libraries = [
+      libx11
+    ];
+  };
   environment.systemPackages = with pkgs; [
     android-studio-full
     flutterPackages.stable
