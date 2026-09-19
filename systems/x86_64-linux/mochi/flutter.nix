0757b3f163d67fcebad61932ac5738d7548501e3
Chxrryo <alina.pollich4@gmail.com>
2026-09-19 19:58:06 +0100

    add deps

--- systems/x86_64-linux/mochi/flutter.nix
+++ systems/x86_64-linux/mochi/flutter.nix
@@ -4,9 +4,37 @@
   programs.nix-ld = {
     enable = true;
     libraries = with pkgs; [
-      libx11
+      glibc
+      libcxx
       libpulseaudio
-      gperftools
+      libtiff
+      libuuid
+      zlib
+      libbsd
+      ncurses5
+      libdrm
+      stdenv.cc.cc
+      expat
+      freetype
+      nss
+      nspr
+      alsa-lib
+      waylandpp.lib
+      libgbm
+      libx11
+      libxext
+      libxdamage
+      libxfixes
+      libxcb
+      libxcomposite
+      libxcursor
+      libxi
+      libxrender
+      libxtst
+      libice
+      libsm
+      libxkbfile
+      libxshmfence
     ];
   };
   environment.systemPackages = with pkgs; [
